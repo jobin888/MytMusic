66289fea3e31ddc8befcc13b9756c77b70b83792
01-Meyitzade-01 <83854378+01-Meyitzade-01@users.noreply.github.com>
2022-03-16 02:26:52 +0300

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -10,6 +10,7 @@
 HELP_1 = """✅**<u>Tüm Komutları:</u>**
 
 /oynat    - şarkıyı oynatır.
+/vplay    - video olarak oynatır.
 /durdur   - Çalan müziği duraklatın.
 /devam    - Duraklatılan müziği devam ettir.
 /atla     - Çalmakta olan müziği atla.
@@ -17,7 +18,7 @@ HELP_1 = """✅**<u>Tüm Komutları:</u>**
 /karistir - Sıraya alınmış çalma listesini rastgele karıştırır.
 /tekrarla - şarkıyı tekrar çalar.
 /bul      - şarkıyı/videoyu indirir.
-/izlet    - canlı yayın akışları oynatır.
+/stream    - canlı yayın akışları oynatır.
 """
 
 HELP_2 = """✅<u>**Sahip:**</u>
@@ -39,7 +40,7 @@ HELP_5 = """🔰**<u>Owner komutları :</u>**
 /addsudo [Username or Reply to a user]
 /delsudo [Username or Reply to a user]
 
-🤖**<u>BOT COMMANDS:</u>**
+🤖**<u>BOT KOMUTLARI:</u>**
 /restart - Restart Bot. 
 /update - Update Bot.
 /speedtest - Check server speeds
@@ -47,39 +48,39 @@ HELP_5 = """🔰**<u>Owner komutları :</u>**
 /logger [enable / disable] - Bot logs the searched queries in logger group.
 /get_log [Number of Lines] - Get log of your bot from heroku or vps. Works for both.
 
-📈**<u>STATS COMMANDS:</u>**
+📈**<u>STATS KOMUTLARI:</u>**
 /activevoice - Check active voice chats on bot.
 /activevideo - Check active video calls on bot.
 /stats - Check Bots Stats
 
-⚠️**<u>BLACKLIST CHAT FUNCTION:</u>**
+⚠️**<u>KARA LİSTE SOHBET FONKSİYONU:</u>**
 /blacklistchat [CHAT_ID] - Blacklist any chat from using Music Bot
 /whitelistchat [CHAT_ID] - Whitelist any blacklisted chat from using Music Bot
 /blacklistedchat - Check all blacklisted chats.
 
-👤**<u>BLOCKED FUNCTION:</u>**
+👤**<u>ENGELLİ FONKSİYON:</u>**
 /block [Username or Reply to a user] - Prevents a user from using bot commands.
 /unblock [Username or Reply to a user] - Remove a user from Bot's Blocked List.
 /blockedusers - Check blocked Users Lists
 
-👤**<u>GBAN FUNCTION:</u>**
+👤**<u>GBAN FONKSİYONU:</u>**
 /gban [Username or Reply to a user] - Gban a user from bot's served chat and stop him from using your bot.
 /ungban [Username or Reply to a user] - Remove a user from Bot's gbanned List and allow him for using your bot
 /gbannedusers - Check Gbanned Users Lists
 
-🎥**<u>VIDEOCALLS FUNCTION:</u>**
+🎥**<u>VİDEO ÇAĞRISI İŞLEVİ:</u>**
 /set_video_limit [Number of Chats] - Set a maximum Number of Chats allowed for Video Calls at a time. Default to 3 chats.
 /videomode [download|m3u8] - If download mode is enabled, Bot will download videos instead of playing them in M3u8 form. ByDefault to M3u8. You can use download mode when any query doesnt plays in m3u8 mode.
 
-⚡️**<u>PRIVATE BOT FUNCTION:</u>**
+⚡️**<u>ÖZEL BOT FONKSİYONU:</u>**
 /authorize [CHAT_ID] - Allow a chat for using your bot.
 /unauthorize [CHAT_ID] - Disallow a chat from using your bot.
 /authorized - Check all allowed chats of your bot.
 
-🌐**<u>BROADCAST FUNCTION:</u>**
+🌐**<u>YAYIN FONKSİYONU:</u>**
 /reklam [Message or Reply to a Message] - Broadcast any message to Bot's Served Chats.
 
-<u>options for broadcast:</u>
+<u>yayın seçenekleri:</u>
 **-pin** : This will pin your message 
 **-pinloud** : This will pin your message with loud notification
 **-user** : This will broadcast your message to the users who have started your bot.
